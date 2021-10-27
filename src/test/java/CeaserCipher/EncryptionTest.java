013b03da9474bd997fb541ab3337bb4281041560
BRIAN MAKAU <brian.mutiso@student.moringaschool.com>
2021-10-27 14:50:59 +0300

    Edit the encryptiontest

--- src/test/java/CeaserCipher/EncryptionTest.java
+++ src/test/java/CeaserCipher/EncryptionTest.java
@@ -7,6 +7,22 @@ import static org.junit.Assert.*;
 public class EncryptionTest {
 
     @Test
-    public void encryption() {
+    public void encryption_testUserInputLetter_String() {
+        Encryption testEncryption = new Encryption();
+        String expectedOutput = "Brian";
+        assertEquals(expectedOutput, testEncryption.encryption("Csjbo", 1));
+    }
+
+    @Test
+    public void encryption_testUserTextOtherCharacters_String(){
+        Encryption testEncryption = new Encryption();
+        String expectedOutput = "Makau";
+        assertEquals(expectedOutput, testEncryption.encryption("Nblbv", 1));
+    }
+    @Test
+    public void encrypt_testUserTextUppercase_String(){
+        Encryption testEncryption = new Encryption();
+        String expectedOutput = "GHL";
+        assertEquals(expectedOutput, testEncryption.encryption("HIM", 1));
     }
 }
